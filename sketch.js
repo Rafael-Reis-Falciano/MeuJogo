829a5203486f48f7a41d45690a465a78b0868ce1
Rafael-Reis-Falciano <94202747+Rafael-Reis-Falciano@users.noreply.github.com>
2022-04-29 19:04:07 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,10 +10,16 @@ var colidiu = false;
 var right = 0;
 var left = 1;
 var direcaoPulo = right;
+var personagem1, personagem2, personagem3, personagem4, personagem5;
 
 function preload()
 {
  fundo = loadImage("imagem fundo.jpg");
+ personagemComeco = loadImage("sprite_0.png");
+ personagemPulandoDireita = loadImage("sprite_1.png");
+ personagemPulandoEsquerda = loadImage("sprite_2.png");
+ personagemParedeDireita = loadImage("sprite_3.png");
+ personagemParedeEsquerda = loadImage("sprite_4.png");
 }
 
 function setup()
@@ -32,15 +38,15 @@ function setup()
     frictionAir: 0.05, 
   }
 
-  paredeEsquerda = Bodies.rectangle(0, 0, 50, 1000, static_options);
-  paredeDireita = Bodies.rectangle(350, 0, 50, 1000, static_options);
+  paredeEsquerda = Bodies.rectangle(0, 0, 30, 1000, static_options);
+  paredeDireita = Bodies.rectangle(350, 0, 30, 1000, static_options);
   World.add(world, paredeEsquerda);
   World.add(world, paredeDireita);
 
   solo = Bodies.rectangle(0, 580, 400, 1, static_options);
   World.add(world, solo);
 
-  player = Bodies.rectangle(185, 70, 30, 40, player_options);
+  player = Bodies.rectangle(185, 70, 50, 50, player_options);
   World.add(world, player);
 
 }
@@ -50,12 +56,14 @@ function draw()
   background("gray");
   Engine.update(engine);
 
-  image(fundo, 0, 0, width, height);
+  //image(fundo, 0, 0, width, height);
 
-  fill("red");
-  rect(player.position.x, player.position.y, 30, 40);
-  /*rect(paredeEsquerda.position.x, paredeEsquerda.position.y, 50, 600);
-  rect(paredeDireita.position.x, paredeDireita.position.y, 50, 600);*/
+  if(player.position.y > 550)
+  {
+  image(personagemComeco, player.position.x, player.position.y, 50, 50);
+  }
+  rect(paredeEsquerda.position.x, paredeEsquerda.position.y, 50, 600);
+  rect(paredeDireita.position.x, paredeDireita.position.y, 50, 600);
 
   colisao(paredeEsquerda);
   colisao(paredeDireita);
@@ -66,19 +74,29 @@ function movimentacao()
 {
   if(keyDown("space") && direcaoPulo === right)
   {
-    Matter.Body.setVelocity(player, {x: 20, y: -10});
+    Matter.Body.setVelocity(player, {x: 12, y: -10});
   }
 
   if(keyDown("space") && direcaoPulo === left)
   {
-    Matter.Body.setVelocity(player, {x: -20, y: -10});
+    Matter.Body.setVelocity(player, {x: -12, y: -10});
+  }
+
+  if(player.position.y < 550 && direcaoPulo === left && player.position.x < 320)
+  {
+    image(personagemPulandoEsquerda, player.position.x, player.position.y, 50, 50);
+  }
+
+  if(player.position.y < 550 && direcaoPulo === right && player.position.x > 60)
+  {
+    image(personagemPulandoDireita, player.position.x, player.position.y, 50, 50);
   }
 }
 
 function colisao(p)
 {
   var colidir = Matter.SAT.collides(player, p);
-  
+
   if(colidir.collided)
   {
     //Matter.Body.setStatic(player, true);
@@ -95,10 +113,12 @@ function colisao(p)
     if(p === paredeEsquerda)
     {
       direcaoPulo = right;
+      image(personagemParedeEsquerda, player.position.x, player.position.y, 50, 50);
     }
     if(p === paredeDireita)
     {
       direcaoPulo = left;
+      image(personagemParedeDireita, player.position.x, player.position.y, 50, 50);
     }
   }
 
